34776dcfff40409b79e3963a795941deb612be0d
PinPex <aleksei-picnhuk0202@mail.ru>
2022-03-03 18:20:31 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        RomanNumber num_1 = new RomanNumber(1500);
+        RomanNumber num_2 = new RomanNumber(2100);
+        RomanNumber num_3 = new RomanNumber(100);
+        RomanNumber num_4 = new RomanNumber(5);
+        RomanNumber num_5 = new RomanNumber(2000);
+        RomanNumber num_6 = new RomanNumber(299);
+        Console.WriteLine("Number №1 = 1500 = " + num_1.ToString());
+        Console.WriteLine("Number №2 = 2100 = " + num_2.ToString());
+        Console.WriteLine("Number №3 = 100 = " + num_3.ToString());
+        Console.WriteLine("Number №4 = 5 = " + num_4.ToString());
+        Console.WriteLine("Number №5 = 2000 = " + num_5.ToString());
+        Console.WriteLine("Number №6 = 299 = " + num_6.ToString());
+        Console.WriteLine("Num№1 + Num№1 + Num№4 = " + (num_1 + num_1 + num_4).ToString());
+        //Console.WriteLine("(num_5 + num_5).ToString()); /*exception*/
+        Console.WriteLine("Num№1 - Num№4 * Num№6 = " + (num_1 - num_4 * num_6).ToString());
+        //Console.WriteLine("(num_5 - num_5).ToString()); /*exception*/
+        Console.WriteLine("Num№3 * Num№4 = " + (num_3 * num_4).ToString());
+        //Console.WriteLine("(num_5 * num_5).ToString()); /*exception*/
+        Console.WriteLine("Num№3 / Num№4 = " + (num_3 / num_4).ToString());
+        Console.WriteLine("Num№3 / Num№3 = " + (num_3 / num_3).ToString());
+
+        Console.WriteLine("\nSort");
+        RomanNumber[] numbers = { num_1, num_2, num_3, num_4, num_6 };
+        Array.Sort(numbers);
+        for (int i = 0; i < 5; ++i)
+        {
+            Console.WriteLine(numbers[i].ToString());
+        }
+        Console.WriteLine("\nCopy");
+        var f = (RomanNumber)num_1.Clone();
+        Console.WriteLine(f.ToString());
+    }
+}
---
+++ ConsoleApp4/RomanNumber.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+internal class RomanNumber : ICloneable, IComparable
+{
+    private int num;
+    private string roman_str;
+    public RomanNumber(int n)
+    {
+        if (n < 1 || n > 3999)
+            throw new RomanNumberException("Uncorrectly number");
+        num = n;
+        roman_str = "";
+        int[] eqv = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string[] roman_symbols = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 13; ++i)
+        {
+            while (num >= eqv[i])
+            {
+                num -= eqv[i];
+                result.Append(roman_symbols[i]);
+            }
+        }
+        roman_str = result.ToString();
+        num = n;
+    }
+
+    public object Clone()
+    {
+        return new RomanNumber(num);
+    }
+
+    public int CompareTo(object? n)
+    {
+        if (n is RomanNumber num)
+            return this.num.CompareTo(num.num);
+        throw new RomanNumberException("Uncorrectly parameter");
+    }
+    public static RomanNumber operator +(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 is null || n2 is null)
+            throw new RomanNumberException("Adding these numbers is impossible");
+        else
+            return new RomanNumber(n1.num + n2.num);
+    }
+    public static RomanNumber operator -(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 is null || n2 is null || n2.num - n1.num > 0)
+            throw new RomanNumberException("Subtracting these numbers is impossible");
+        else
+            return new RomanNumber(n1.num - n2.num);
+    }
+    public static RomanNumber operator *(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 is null || n2 is null)
+            throw new RomanNumberException("Multiplying these numbers is impossible");
+        else
+            return new RomanNumber(n1.num * n2.num);
+    }
+    public static RomanNumber operator /(RomanNumber? n1, RomanNumber? n2)
+    {
+        if (n1 is null || n2 is null || n2.num / n1.num > 1)
+            throw new RomanNumberException("Dividing these numbers");
+        else
+            return new RomanNumber(n1.num / n2.num);
+    }
+    public override string ToString()
+    {
+        if (roman_str != "")
+            return roman_str;
+        else
+            throw new RomanNumberException("Number has not found");
+    }
+}
